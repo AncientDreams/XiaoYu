f9f528311976d73c836e4a1a150ae5f2aeede618
AncientDreams <18574949413@sina.cn>
2020-08-28 16:28:02 +0800

    Initial commit

---
+++ xiaoyu-auth/src/test/java/com/xiaoyu/auth/XiaoyuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoyuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
